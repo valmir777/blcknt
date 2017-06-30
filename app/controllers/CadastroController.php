55838f5536307f6deb675a89690c5619a236163b
Valmir Nascimento <valmirmarketing@gmail.com>
2017-06-30 12:56:50 -0300

    Criação de Controllers e Views para Cadastro, login e Recuperação

---
+++ app/controllers/CadastroController.php
@@ -0,0 +1,8 @@
+<?php
+
+//Crie o Objeto CadastrController.
+//Esse Objeto precisa extender a classe mestre principal Controller
+class CadastroController extends \HXPHP\System\Controller
+{
+
+}
---
+++ app/controllers/RecuperarController.php
@@ -0,0 +1,8 @@
+<?php
+
+//Crie o Objeto CadastrController.
+//Esse Objeto precisa extender a classe mestre principal Controller
+class RecuperarController extends \HXPHP\System\Controller
+{
+
+}
